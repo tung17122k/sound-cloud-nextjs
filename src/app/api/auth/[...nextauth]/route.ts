1dd57f21d1810c0526bb511e2a3cff5cc7f9a042
tung17122k <sanking1712@gmail.com>
2024-12-26 18:38:52 +0700

    route auth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,20 @@
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+import { AuthOptions } from "next-auth"
+
+export const authOptions: AuthOptions = {
+    // Configure one or more authentication providers
+    secret: process.env.NO_SECRET,
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID!,
+            clientSecret: process.env.GITHUB_SECRET!,
+        }),
+        // ...add more providers here
+    ],
+}
+
+
+const handler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST }
