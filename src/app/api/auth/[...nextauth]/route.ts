c110c71920398fdc1ef29e1d6e0c95ae13fa61b1
tung17122k <sanking1712@gmail.com>
2024-12-27 15:46:25 +0700

    signin with credentials, github

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,27 +1,92 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import { AuthOptions } from "next-auth"
+import { sendRequestJS } from "@/utils/api"
+import { JWT } from "next-auth/jwt"
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions: AuthOptions = {
     // Configure one or more authentication providers
     secret: process.env.NO_SECRET,
     providers: [
+
+        // ...add more providers here
+        CredentialsProvider({
+            // The name to display on the sign in form (e.g. "Sign in with...")
+            name: "Credentials",
+            // `credentials` is used to generate a form on the sign in page.
+            // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+            // e.g. domain, username, password, 2FA token, etc.
+            // You can pass any HTML attribute to the <input> tag through the object.
+            credentials: {
+                username: { label: "Tên đăng nhập", type: "text", },
+                password: { label: "Mật khẩu", type: "password" }
+            },
+            async authorize(credentials, req) {
+                // Add logic here to look up the user from the credentials supplied
+                // const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+                const res = await sendRequestJS<IBackendRes<JWT>>({
+                    url: "http://localhost:8000/api/v1/auth/login",
+                    method: "POST",
+                    body: {
+                        username: credentials?.username,
+                        password: credentials?.password
+                    }
+                })
+                if (res && res.data) {
+                    return res.data as any;
+                } else {
+                    return null
+                }
+
+
+            }
+        }),
         GithubProvider({
             clientId: process.env.GITHUB_ID!,
             clientSecret: process.env.GITHUB_SECRET!,
         }),
-        // ...add more providers here
     ],
     callbacks: {
         async jwt({ token, user, account, profile, trigger }) {
-            if (trigger === "signIn" && account?.provider === "github") {
-                token.address = "Ha Noi"
+            if (trigger === "signIn" && account?.provider !== "credentials") {
+                const res = await sendRequestJS<IBackendRes<JWT>>({
+                    url: "http://localhost:8000/api/v1/auth/social-media",
+                    method: "POST",
+                    body: {
+                        type: account?.provider.toUpperCase(),
+                        username: user.email
+                    }
+                })
+
+                if (res.data) {
+                    token.access_token = res.data.access_token;
+                    token.refresh_token = res.data.refresh_token;
+                    token.user = res.data.user;
+
+                }
+            }
+            if (trigger === "signIn" && account?.provider === "credentials") {
+                // local data
+                //@ts-ignore
+                token.access_token = user.access_token;
+                //@ts-ignore
+                token.refresh_token = user.refresh_token;
+                //@ts-ignore
+                token.user = user.user;
             }
             return token
+
+
         },
+        // gan session cho sever
         async session({ session, token, user }) {
-            //@ts-ignore
-            session.address = token.address;
+            if (token) {
+                session.access_token = token.access_token;
+                session.refresh_token = token.refresh_token;
+                session.user = token.user;
+            }
+
             return session
         }
     }
