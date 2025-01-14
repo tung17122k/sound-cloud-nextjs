90a985fa608a953d0472211074b68433f9f2063b
tung17122k <sanking1712@gmail.com>
2025-01-14 15:28:18 +0700

    update slug rename to id

--- src/app/(user)/track/[slug]/page.tsx
+++ src/app/(user)/track/[slug]/page.tsx
@@ -56,7 +56,7 @@ const DetailTrackPage = async (props: any) => {
         queryParams: {
             current: 1,
             pageSize: 100,
-            trackId: params.slug,
+            trackId: id,
             sort: "-createdAt"
         }
     })
